deab35000a9fe13b08ec92b07a6349a7519c8b17
ireolaniyan <ireolaniyan7@gmail.com>
2019-08-03 22:36:08 +0100

    chore: Create TravelDeals POJO class. Create database reference.

---
+++ app/src/main/java/com/example/travelmantics/Models/TravelDeals.java
@@ -0,0 +1,53 @@
+package com.example.travelmantics.Models;
+
+public class TravelDeals {
+    private String id, title, price, description, imageUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public TravelDeals(String id, String title, String price, String description, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+}
