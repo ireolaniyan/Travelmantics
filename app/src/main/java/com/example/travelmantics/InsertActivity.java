7aabe16ee317a1aec2bd9e936a6092bca26042f3
ireolaniyan <ireolaniyan7@gmail.com>
2019-08-03 22:47:12 +0100

    wip(InsertActivity): get the edit text fields created in the insert activity

--- app/src/main/java/com/example/travelmantics/InsertActivity.java
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -1,10 +1,13 @@
 package com.example.travelmantics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -12,6 +15,9 @@ import com.google.firebase.database.FirebaseDatabase;
 public class InsertActivity extends AppCompatActivity {
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
+    private EditText textTitle;
+    private EditText textPrice;
+    private EditText textDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,15 @@ public class InsertActivity extends AppCompatActivity {
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+
+        textTitle = findViewById(R.id.text_title);
+        textPrice = findViewById(R.id.text_price);
+        textDescription = findViewById(R.id.text_description);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
