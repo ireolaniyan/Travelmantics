6e27ffd394ecd9a7fd1e55a091be0e563c5489db
ireolaniyan <ireolaniyan7@gmail.com>
2019-08-03 22:19:35 +0100

    ui: add title, price and description edit text fields to InsertActivity. Add Save menu.

--- app/src/main/java/com/example/travelmantics/InsertActivity.java
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -3,12 +3,22 @@ package com.example.travelmantics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class InsertActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_insert);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        Show Save Menu
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.save_menu, menu);
+        return true;
     }
 }
