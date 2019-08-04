90277c0d5a9d744c12b049c4e9950dded7665bd3
ireolaniyan <ireolaniyan7@gmail.com>
2019-08-04 13:06:42 +0100

    wip: Add List activity class and view

---
+++ app/src/main/java/com/example/travelmantics/ListActivity.java
@@ -0,0 +1,25 @@
+package com.example.travelmantics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.travelmantics.Models.TravelDeal;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class ListActivity extends AppCompatActivity {
+    ArrayList<TravelDeal> deals;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mChildListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
