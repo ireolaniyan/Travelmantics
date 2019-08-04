796fd350929cb36298fefd137c23b2ccfb3a0d6a
ireolaniyan <ireolaniyan7@gmail.com>
2019-08-04 22:01:55 +0100

    feat(authentication): Logout

--- app/src/main/java/com/example/travelmantics/ListActivity.java
+++ app/src/main/java/com/example/travelmantics/ListActivity.java
@@ -7,11 +7,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.example.travelmantics.Utils.FirebaseUtil;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ListActivity extends AppCompatActivity {
@@ -47,6 +51,19 @@ public class ListActivity extends AppCompatActivity {
             case R.id.insert_menu:
                 Intent intent = new Intent(this, DealActivity.class);
                 startActivity(intent);
+                return true;
+
+            case R.id.logout_menu:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.d("Logout", "user Logged Out");
+//                                Go back to login page
+                                FirebaseUtil.attachlistener();
+                            }
+                        });
+                FirebaseUtil.detachlistener();
                 return true;
         }
         return super.onOptionsItemSelected(item);
